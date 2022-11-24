a13ce29da7e0971fafb8013c5484cd19fdedaeda
Istvan Haller <ihaller@microsoft.com>
2022-11-24 18:29:38 +0000

    Added check to avoid overloading COM port

--- guest-verona-rt/arch/x86_64/serial.cc
+++ guest-verona-rt/arch/x86_64/serial.cc
@@ -8,6 +8,10 @@ namespace monza
 {
   void uartputc_generic(uint8_t c)
   {
+    // Wait until transmitter holding register is signalled as empty.
+    while ((in<uint8_t>(COM1 + 5) & (1 << 5)) == 0)
+      ;
+
     out<uint8_t>(c, COM1);
   }
 }
