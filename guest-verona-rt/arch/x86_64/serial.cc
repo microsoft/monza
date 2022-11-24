5cf2f9fce99489866c27dee195e5c4fa6f4f5af3
Istvan Haller <ihaller@microsoft.com>
2022-11-24 19:13:00 +0000

    Improved documentation

--- guest-verona-rt/arch/x86_64/serial.cc
+++ guest-verona-rt/arch/x86_64/serial.cc
@@ -8,8 +8,15 @@ namespace monza
 {
   void uartputc_generic(uint8_t c)
   {
+    /**
+     * Based on: https://wiki.osdev.org/Serial_Ports
+     */
+    constexpr uint16_t LINE_STATUS_REGISTER_OFFSET = 5;
+    constexpr uint8_t TRANSMIT_HOLDING_REGISTER_EMPTY_MASK = 1 << 5;
+
     // Wait until transmitter holding register is signalled as empty.
-    while ((in<uint8_t>(COM1 + 5) & (1 << 5)) == 0)
+    while ((in<uint8_t>(COM1 + LINE_STATUS_REGISTER_OFFSET) &
+            TRANSMIT_HOLDING_REGISTER_EMPTY_MASK) == 0)
       ;
 
     out<uint8_t>(c, COM1);
